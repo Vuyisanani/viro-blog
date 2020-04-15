5b9b4690901d36eb3c8d90e5ac594be1ad08cd00
Vuyisanani <vmeteni@gmail.com>
2020-04-15 10:48:34 +0200

    Removed Comments

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-// import { Observable } from 'rxjs';
 import {
   AngularFirestore,
   AngularFirestoreCollection,
@@ -14,17 +13,14 @@ import { map } from 'rxjs/operators';
 @Injectable()
 export class PostService {
   postsCollection: AngularFirestoreCollection<Post>;
-  //Post Doc not Callable ( To: Do)
   postDoc: AngularFirestoreDocument<Post>;
-  // afs: Angular Fire store
+ 
   constructor(private afs: AngularFirestore) {
-    // Linking the collection to the 'posts' collection created in Firebase db
     this.postsCollection = this.afs.collection('posts', ref =>
     ref.orderBy('published', 'desc'));
    }
 
    public getPosts(): Observable<Post[]> {
-     //To Do: use diff map method
      const collectChanges = this.postsCollection.snapshotChanges();
      return collectChanges.pipe(map( actions => {
       return actions.map(res => {
@@ -34,20 +30,12 @@ export class PostService {
            })
      }))
      
-    //  .map(actions => {
-    //    return actions.map(res => {
-    //      const data = res.payload.doc.data() as Post;
-    //      const id = res.payload.doc.id;
-    //      return { id, ...data };
-    //    })
-    //  })
      }
    getPostData(id: string) {
     this.postDoc = this.afs.doc<Post>(`post:/${id}`);
     return this.postDoc.valueChanges();
    }
 
-   // CRUD functions
    getPost(id: string) {
      return this.afs.doc<Post>(`posts/${id}`)
    }
